454050ce662b5dd4f4c72606830420caf983ba4f
roy <alexl@evilnut.ca>
2015-01-07 17:53:05 -0800

    second

---
+++ MKbuyer/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/evilnut/onlinestore/shop2/demo1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/evilnut/onlinestore/shop2/demo1/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\evilnut\onlinestore\shop2\demo1/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqliz');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop2demo1');
+define('DB_PREFIX', 'oc_');
+?>
